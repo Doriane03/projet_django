7c4214a849670e10a6139cd80eb27cb8eda33494
Doriane03 <josephinedorianekouadio@gmail.com>
2024-07-13 17:09:45 +0000

    dsfdr

--- stage_projet/listings/views.py
+++ stage_projet/listings/views.py
@@ -155,11 +155,9 @@ def index(request):
 def patient(request):
     lits = lit.objects.all()
     if request.method == 'POST':
-        var=request.POST['numlit']
-        #Lit_id= lit.objects.filter(numlit=request.POST['numlit']).values_list('reflit', flat=True).first()
-        print(var)
-        #reg=personnel_soignant(mdp=mdp,nom=nom,contact=contact,email=email,service_id=Service_id, type_personnel_soignant_id= Type_personnel_soignant_id)
-        #reg.save()
+        Lit_id= lit.objects.filter(numlit=request.POST['numlit']).values_list('reflit', flat=True).first()
+        reg=patient(mdp=mdp,nom=nom,contact1=request.POST['contact1'],contact2=request.POST['contact2'],email=request.POST['email'],personne_a_contacter=request.POST['personne_a_contacter'],telephone_cpu=request.POST['telephone_cpu'],date_naissance=request.POST['date_naissance'],profession=request.POST['profession'],ville=request.POST['ville'],age=request.POST['age'],sexe=request.POST['sexe'],commune=request.POST['commune'],quartier=request.POST['quartier'],nationalite=request.POST['nationalite'],situation_matrimoniale=request.POST['situation_matrimoniale'],nombre_enfant=request.POST['nombre_enfant'],lit_id=Lit_id)
+        reg.save()
     return render(request,'listings/formpatient.html',context={'lits':lits })
 #fin
 def constante(request):
