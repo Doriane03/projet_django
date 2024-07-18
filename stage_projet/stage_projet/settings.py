fcc15a25226d1276157ead89aa0b4b61d1ee4220
Doriane03 <josephinedorianekouadio@gmail.com>
2024-07-18 23:36:42 +0000

    vchg

--- stage_projet/stage_projet/settings.py
+++ stage_projet/stage_projet/settings.py
@@ -18,7 +18,7 @@ BASE_DIR =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
 environ.Env.read_env()
-LOGIN_URL = ''
+LOGIN_URL = '/'
 LOGOUT_URL = '/'  # URL pour la déconnexion
 LOGIN_REDIRECT_URL = 'menu'  # URL de redirection après connexion réussie
 LOGOUT_REDIRECT_URL = 'index'
