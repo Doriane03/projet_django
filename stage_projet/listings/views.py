4408c0fa55346155755d02daf8b6514c82eef06b
Doriane03 <josephinedorianekouadio@gmail.com>
2024-07-31 17:17:11 +0000

    BCCHFG

--- stage_projet/listings/views.py
+++ stage_projet/listings/views.py
@@ -345,7 +345,7 @@ def docpatient(request):
 
     if doc == 'ok' and pk:
         # Affiche le template affichedocpatient.html pour un patient spécifique
-        patient = get_object_or_404(Patient, idpatient=pk)
+        patient = get_object_or_404(Patient, idpatient=pk) 
         return render(request, 'listings/affichagedocpatient.html', {'patient': patient})
 
     query = request.GET.get('query', '')
