d45a76929690d59ae2bf23cfe1069c069af32bd3
Doriane03 <josephinedorianekouadio@gmail.com>
2024-09-11 13:51:08 +0000

    CELERY

--- stage_projet/test_task.py
+++ stage_projet/test_task.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from django.conf import settings
 
 # Ajouter le répertoire du projet au PYTHONPATH
 sys.path.append('/home/doriane/projet_django/stage_projet')
@@ -11,11 +10,10 @@ import django
 django.setup()
 
 # Maintenant, vous pouvez importer les modèles
-from stage_projet.celery import app
 from stage_projet.tasks import relance
 
 # Enqueue la tâche pour l'exécuter
 result = relance.delay()
 
 # Afficher le résultat de la tâche
-print(result.get(timeout=1))  # Attendre jusqu'à 10 secondes pour obtenir le résultat
+print(result.get(timeout=10))  # Attendre jusqu'à 10 secondes pour obtenir le résultat
